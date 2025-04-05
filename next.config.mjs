ae4e2f00a747a16d58eee9d5fbf52319138f5d45
Mohd Danish <mddanishyusuf@gmail.com>
2025-04-05 05:59:59 +0530

    good work

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (config, { dev, isServer }) => {
-        if (dev && !isServer) {
-            // Disable HMR
-            config.plugins = config.plugins.filter((plugin) => plugin.constructor.name !== 'HotModuleReplacementPlugin');
-        }
-        return config;
-    },
 };
 
 export default nextConfig;
